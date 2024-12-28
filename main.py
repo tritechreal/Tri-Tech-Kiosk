c85dfe99769cfe76b559cb93ad0ac1e715a17b1c
TheCubeFactor <78697204+TheCubeFactor@users.noreply.github.com>
2024-12-28 10:23:43 -0500

    Added Acount menu but need for finish the actual account stuff

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-# Create both screens. Please note the root.manager.current: this is how
+# Create  screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
 # property manager that gives you the instance of the ScreenManager used.
 Builder.load_string("""
@@ -13,7 +13,7 @@ Builder.load_string("""
             on_press: root.manager.current = 'settings'
         Button:
             text: 'Account'
-            on_press: root.manager.current = 'login'
+            on_press: root.manager.current = 'Account'
         Button:
             text: 'Quit'
 
@@ -27,13 +27,41 @@ Builder.load_string("""
 <Account>:
     BoxLayout:
         Button:
-            text: 'Login'
+            text: 'Sign In'
+            on_press: root.manager.current = 'sign_in'
+        Button:
+            text: 'Sign Up'
+            on_press: root.manager.current = 'sign_up'
+        Button:
+            text: 'Back to menu'
+            on_press: root.manager.current = 'menu'
+                    
+<Sign_In>:
+    BoxLayout:
+        Button:
+            text: 'Email'
+            
+        Button:
+            text: 'Password'
+    
+        Button:
+            text: 'Back to menu'
+            on_press: root.manager.current = 'menu'
+
+<Sign_Up>:
+    BoxLayout:
+        Button:
+            text: 'Email'
+            
+        Button:
+            text: 'Password'
+    
         Button:
             text: 'Back to menu'
             on_press: root.manager.current = 'menu'
 """)
 
-# Declare both screens
+# Declare all screens
 class MenuScreen(Screen):
     pass
 
@@ -42,6 +70,13 @@ class SettingsScreen(Screen):
 
 class Account(Screen):
     pass
+
+class Sign_In(Screen):
+    pass
+
+class Sign_Up(Screen):
+    pass
+
 class TestApp(App):
 
     def build(self):
@@ -49,7 +84,9 @@ class TestApp(App):
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(SettingsScreen(name='settings'))
-        sm.add_widget(Account(name='login'))
+        sm.add_widget(Account(name='Account'))
+        sm.add_widget(Sign_In(name='sign_in'))
+        sm.add_widget(Sign_Up(name='sign_up'))
 
         return sm
 
