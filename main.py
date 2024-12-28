cf48d48ee68beacd544f1d2bcf5c9f336ddc0dfd
TheCubeFactor <78697204+TheCubeFactor@users.noreply.github.com>
2024-12-27 20:06:29 -0500

    Buttons do stuff now, needs work to figure out what button dose work

--- main.py
+++ main.py
@@ -14,11 +14,28 @@ screen = Screen(name='mainpage')
 class TestApp(App):
 
     def build(self):
-        grid = GridLayout(cols=4, rows=1)
-        grid.add_widget(Button(text="Upload Fish"))
-        grid.add_widget(Button(text="Leaderboard"))
-        grid.add_widget(Button(text="Fish Stats"))
-        grid.add_widget(Button(text="Sign in"))
+        grid = GridLayout(cols=5, rows=1) #defigns the rows and coloms for the grid of this screen
+
+        def callback(instance, value):
+            print('My button <%s> state is <%s>' % (instance, value)) #figure this out pls
+            if instance == "0x0000017ABF501710": #This line dosent work
+                if value == "down":
+                    print("Goto Signing Screen")
+        btn1 = Button(text='Hello world 1')
+        btn1.bind(state=callback)
+
+
+        sign_in = Button(text='Sign In')
+        sign_in.bind(state=callback)
+        #These are not needed so they can be gone
+        
+        #grid.add_widget(Button(text="Upload Fish"))
+        #grid.add_widget(Button(text="Leaderboard"))
+        #grid.add_widget(Button(text="Fish Stats"))
+        #grid.add_widget(Button(text="Sign in"))
+        
+        grid.add_widget(btn1)
+        grid.add_widget(sign_in)
         
         screen = Screen(name='sign in')
         sm.add_widget(screen)
