08a054556d70e1595c77ee66709aa4d5ff21c274
cube <75490449+galileoleosolis@users.noreply.github.com>
2025-01-05 16:58:58 -0500

    fish

--- record.py
+++ record.py
@@ -6,31 +6,28 @@
 from sys import stdin
 from termios import TCIOFLUSH, tcflush
 from time import strftime
-
+import time
 from keyboard import is_pressed
 
 from picamera2 import Picamera2, Preview
-
-key_flag = False
 cam = Picamera2()
+
+cam.start
 cam.start_preview(Preview.QTGL)
 cam.start()
 
-try:
-    while True:
-        if is_pressed('p'):
-            if key_flag is False:
-                key_flag = True
-            filename = strftime("%Y%m%d-%H%M%S") + '.png'
-            cam.capture_file("/data", filename, format="png", wait=None)
-            print(f"\rCaptured {filename} succesfully")
-        else:
-            key_flag = 0
-        if is_pressed('q'):
-            print("\rClosing camera...")
-            break
-finally:
-    cam.stop_preview()
-    cam.stop()
-    cam.close()
-    tcflush(stdin, TCIOFLUSH)
+
+for i in range(1,50):
+    num = str(i)
+    time.sleep(0.2)
+    filename = "data/" + num + "fish" + '.png'
+    cam.capture_file(filename, format="png", wait=None)
+    print(f"\rCaptured {filename} succesfully")
+    
+
+
+
+cam.stop_preview()
+cam.stop()
+cam.close()
+tcflush(stdin, TCIOFLUSH)
